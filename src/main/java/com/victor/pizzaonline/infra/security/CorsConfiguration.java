1fa24f827bfa63c75b7f172411d7c476b75556fe
Victor <victorhugomourabarreto1@gmail.com>
2024-04-01 16:45:22 -0300

    Configurando cors

--- src/main/java/com/victor/pizzaonline/infra/security/CorsConfiguration.java
+++ src/main/java/com/victor/pizzaonline/infra/security/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://pizza-online-frontend.vercel.app")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*")
                 .allowCredentials(true);
