fb2e038a9f8a9f4fb68a37ed6273a41d2fe21b84
Victor <victorhugomourabarreto1@gmail.com>
2024-05-19 07:26:54 -0300

    config

--- src/main/java/com/victor/pizzaonline/infra/security/CorsConfiguration.java
+++ src/main/java/com/victor/pizzaonline/infra/security/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500/")
+                .allowedOrigins("http://127.0.0.1:5500/", "https://delightful-island-069d02b0f.5.azurestaticapps.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*")
                 .allowCredentials(true);
