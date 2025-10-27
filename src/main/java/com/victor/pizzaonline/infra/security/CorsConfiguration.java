6360c60914413116221ade1073a75a2105f3c693
Marcos Roosevelt <63478082+MarcosRoosevelt@users.noreply.github.com>
2025-10-27 20:37:02 -0300

    alterando o cors

--- src/main/java/com/victor/pizzaonline/infra/security/CorsConfiguration.java
+++ src/main/java/com/victor/pizzaonline/infra/security/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500/", "https://pizzaonline-cdbnfabqbgdcasa9.brazilsouth-01.azurewebsites.net", "https://gentle-sky-0f515510f.3.azurestaticapps.net")
+                .allowedOrigins("http://127.0.0.1:5500", "https://pizzaonline-cdbnfabqbgdcasa9.brazilsouth-01.azurewebsites.net", "https://gentle-sky-0f515510f.3.azurestaticapps.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*")
                 .allowCredentials(true);
